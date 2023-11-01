7e9a5369aa58e58e6c898f4a3f6353d93b26e75b
Anthony <144498068+Anthonyxd22@users.noreply.github.com>
2023-10-31 20:52:54 -0400

    Add files via upload
    Signed-off-by: Anthony <144498068+Anthonyxd22@users.noreply.github.com>

---
+++ Applio🍏/Applio__App.swift
@@ -0,0 +1,17 @@
+//
+//  Applio__App.swift
+//  Applio🍏
+//
+//  Created by anthony on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Applio__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Applio🍏/ContentView.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var modelURL = ""
+    @State private var audioFileURL: URL?
+    @State private var selectedAlgorithm = "pm"
+    @State private var outputAudio: URL?
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Inferencia")) {
+                    TextField("URL del modelo", text: $modelURL)
+                    Button("Cargar archivo de audio") {
+                       
+                    }
+                    if let audioFileURL = audioFileURL {
+                        Text("Archivo de audio cargado: \(audioFileURL.lastPathComponent)")
+                    }
+                    Picker("Algoritmo", selection: $selectedAlgorithm) {
+                        Text("pm").tag("pm")
+                        Text("harvest").tag("harvest")
+                      
+                    }
+                    Button("Convertir") {
+                     
+                    }
+                }
+
+                Section(header: Text("Salida")) {
+                    if outputAudio != nil {
+                        Text("Audio de salida generado.")
+                        Button("Descargar Audio") {
+                           
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Simple RVC Inference")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
